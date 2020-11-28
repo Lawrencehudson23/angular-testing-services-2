39aa93aa9d050532e1c1d45fb80d6d54441e7b3d
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-11-28 03:03:06 -0800

    feat: implement switch active function

---
+++ src/app/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CounterService } from './counter.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
+  constructor(private counterService: CounterService) {}
+  userSetToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+    this.counterService.incrementActiveToInactive();
+  }
+  userSetToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+    this.counterService.incrementInactiveToActive();
+  }
+}
